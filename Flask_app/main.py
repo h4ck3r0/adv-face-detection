9e6b460079eba84de4b2d8b7caab1bc80274915a
Raj Aryan <46929618+h4ck3r0@users.noreply.github.com>
2025-06-17 19:51:28 +0530

    Update main.py

--- Flask_app/main.py
+++ Flask_app/main.py
@@ -10,5 +10,6 @@ app.add_url_rule(rule='/app/gender/',
                  view_func=views.genderapp,
                  methods=['GET','POST'])
 
+
 if __name__ == "__main__":
     app.run(debug=True)
